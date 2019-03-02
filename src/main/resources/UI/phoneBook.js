71e23c80099d4141722de6513c99dc30e8e97ebe
laxmipooja anegundi <laxmipoojaanegundi@laxmipoojas-mbp.home>
2019-03-02 02:58:01 -0800

    second commit

--- src/main/resources/UI/phoneBook.js
+++ src/main/resources/UI/phoneBook.js
@@ -1,9 +1,20 @@
 var myApp = angular.module("PhoneBook",['ngMaterial']);
 myApp.controller('ContactController',function EventController($scope,$http,$compile){
-    var groups = [];
+    $scope.AddNewContacts = false;
+    $scope.ListGroups = false;
+    $http.get('http://localhost:4567/group').then(function (data) {
+        $scope.groups = [];
+        console.log(data.data);
+        angular.forEach(data.data, function (value, index) {
+            $scope.groups.push({ id: value.group_id, label: value.group_name });
+        });
+    })
+    
+    
+    
 //    $http({
 //            method: 'GET',
-//            url: '/someUrl'
+//            url: 'http://localhost:4567/contact'
 //            }).then(function successCallback(response) {
 //            angular.forEach(data.data, function (value) {
 //            $scope.groups.push({ id: value.group_id, label: value.group_name });
@@ -11,8 +22,14 @@ myApp.controller('ContactController',function EventController($scope,$http,$comp
 //            }, function errorCallback(response) {
 //            alert(response.data);
 //            });
-    
-    
+    $scope.Add_Contacts = function(){
+    $scope.AddNewContacts = true;
+    $scope.ListGroups = false;
+}
+    $scope.List_Group=function(){
+        $scope.ListGroups = true;
+        $scope.AddNewContacts = false;
+    }
     
     
     
