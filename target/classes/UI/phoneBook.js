70e8ea54c1437e3ac615d95698ae5a36d2a5d8ab
laxmipooja anegundi <laxmipoojaanegundi@laxmipoojas-mbp.home>
2019-03-02 02:03:08 -0800

    First Commit

---
+++ target/classes/UI/phoneBook.js
@@ -0,0 +1,26 @@
+var myApp = angular.module("PhoneBook",['ngMaterial']);
+myApp.controller('ContactController',function EventController($scope,$http,$compile){
+    var groups = [];
+//    $http({
+//            method: 'GET',
+//            url: '/someUrl'
+//            }).then(function successCallback(response) {
+//            angular.forEach(data.data, function (value) {
+//            $scope.groups.push({ id: value.group_id, label: value.group_name });
+//            });
+//            }, function errorCallback(response) {
+//            alert(response.data);
+//            });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
