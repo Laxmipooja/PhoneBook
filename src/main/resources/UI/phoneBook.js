ce95a846da6c7d19368d9ca28b95306a7c32baaa
laxmipooja anegundi <laxmipoojaanegundi@laxmipoojas-mbp.home>
2019-03-02 10:00:36 -0800

    Third commit

--- src/main/resources/UI/phoneBook.js
+++ src/main/resources/UI/phoneBook.js
@@ -2,42 +2,61 @@ var myApp = angular.module("PhoneBook",['ngMaterial']);
 myApp.controller('ContactController',function EventController($scope,$http,$compile){
     $scope.AddNewContacts = false;
     $scope.ListGroups = false;
-    $http.get('http://localhost:4567/group').then(function (data) {
+    $scope.DisplayAll=true;
+    $http.get('http://localhost:4567/group').then(function (data){
         $scope.groups = [];
         console.log(data.data);
         angular.forEach(data.data, function (value, index) {
             $scope.groups.push({ id: value.group_id, label: value.group_name });
         });
     })
-    
-    
-    
-//    $http({
-//            method: 'GET',
-//            url: 'http://localhost:4567/contact'
-//            }).then(function successCallback(response) {
-//            angular.forEach(data.data, function (value) {
-//            $scope.groups.push({ id: value.group_id, label: value.group_name });
-//            });
-//            }, function errorCallback(response) {
-//            alert(response.data);
-//            });
+    $http({
+            method: 'GET',
+            url: 'http://localhost:4567/contact'
+            }).then(function(response) {
+            Table_AllContacts(response.data);
+            }, function(response) {
+            No_Data();
+            });
     $scope.Add_Contacts = function(){
-    $scope.AddNewContacts = true;
-    $scope.ListGroups = false;
-}
+        $scope.AddNewContacts = true;
+        $scope.ListGroups = false;
+        $scope.DisplayAll = false;
+    }
     $scope.List_Group=function(){
         $scope.ListGroups = true;
         $scope.AddNewContacts = false;
+        $scope.DisplayAll = false;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    $scope.save_Contacts=function(){
+        $scope.AddNewContacts = false;
+        $scope.ListGroups = false;
+        $scope.DisplayAll = true;
+    }
+     $scope.Cancel_Contacts=function(){
+        $scope.AddNewContacts = false;
+        $scope.ListGroups = false;
+        $scope.DisplayAll = true;
+    }
 });
+
+function Table_AllContacts(data){
+    console.log(data[0]);
+    Table = document.createElement("table");
+    Table.className = "table";
+    TableHead = document.createElement("thead");
+    HeadRow = document.createElement('tr');
+    HeadColumn1=document.createElement("th");
+    HeadColumn1.appendChild(document.createTextNode("CONTACTS"))
+    HeadRow.appendChild(HeadColumn1);
+    TableHead.appendChild(HeadRow);
+    Tbody = document.createElement("tbody")
+    for(let contact of data){
+       let trow = document.createElement('tr');
+       let tcol = document.createElement('td');
+       let text = document.createTextNode(contact.Firstname)
+        
+        
+    }
+    Table.appendChild(Tbody);
+}
--- target/classes/UI/phoneBook.js
+++ target/classes/UI/phoneBook.js
@@ -2,33 +2,41 @@ var myApp = angular.module("PhoneBook",['ngMaterial']);
 myApp.controller('ContactController',function EventController($scope,$http,$compile){
     $scope.AddNewContacts = false;
     $scope.ListGroups = false;
-    $http.get('http://localhost:4567/group').then(function (data) {
+    $scope.DisplayAll=true;
+    $http.get('http://localhost:4567/group').then(function (data){
         $scope.groups = [];
         console.log(data.data);
         angular.forEach(data.data, function (value, index) {
             $scope.groups.push({ id: value.group_id, label: value.group_name });
         });
     })
-    
-    
-    
-//    $http({
-//            method: 'GET',
-//            url: 'http://localhost:4567/contact'
-//            }).then(function successCallback(response) {
-//            angular.forEach(data.data, function (value) {
-//            $scope.groups.push({ id: value.group_id, label: value.group_name });
-//            });
-//            }, function errorCallback(response) {
-//            alert(response.data);
-//            });
+    $http({
+            method: 'GET',
+            url: 'http://localhost:4567/contact'
+            }).then(function(response) {
+            Table_AllContacts(response.data);
+            }, function(response) {
+            No_Data();
+            });
     $scope.Add_Contacts = function(){
-    $scope.AddNewContacts = true;
-    $scope.ListGroups = false;
-}
+        $scope.AddNewContacts = true;
+        $scope.ListGroups = false;
+        $scope.DisplayAll = false;
+    }
     $scope.List_Group=function(){
         $scope.ListGroups = true;
         $scope.AddNewContacts = false;
+        $scope.DisplayAll = false;
+    }
+    $scope.save_Contacts=function(){
+        $scope.AddNewContacts = false;
+        $scope.ListGroups = false;
+        $scope.DisplayAll = true;
+    }
+     $scope.Cancel_Contacts=function(){
+        $scope.AddNewContacts = false;
+        $scope.ListGroups = false;
+        $scope.DisplayAll = true;
     }
     
     
@@ -41,3 +49,7 @@ myApp.controller('ContactController',function EventController($scope,$http,$comp
     
     
 });
+
+function Table_AllContacts(data){
+    console.log(data[0])
+}
